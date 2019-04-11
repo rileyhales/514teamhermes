359e39002033d9d53b788a72ede52e8104a22f43
rileyhales <rileyhales1@gmail.com>
2019-04-11 16:28:58 -0600

    drawing controls and erase drawings button. upgrade to arcgis js 4.11

--- tethysapp/teamhermes/public/js/router.js
+++ tethysapp/teamhermes/public/js/router.js
@@ -27,10 +27,26 @@ $(document).ready(function () {
         "esri/views/MapView",
         "esri/layers/MapImageLayer",
         "esri/widgets/LayerList",
+        'esri/views/draw/Draw',
+        "esri/geometry/geometryEngine",
+        "esri/widgets/Sketch",
     ], function (Map, GraphicsLayer, Graphic, Point, Geoprocessor, LinearUnit, FeatureSet, MapView, MapImageLayer,
-                 LayerList) {
+                 LayerList, Draw, geometryEngine, Sketch) {
 
-        // Layers
+        //////////////////////////////////////////////////////////////////////////// MAP SETUP
+        // The map with basemap
+        const map = new Map({
+            basemap: "streets",
+        });
+
+        const view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: [-111.647070, 40.251468], //40.251468, -111.647070
+            zoom: 10
+        });
+
+        //////////////////////////////////////////////////////////////////////////// EMS, FIRE, POLICE LAYERS
         const policeLayerURL = "http://geoserver2.byu.edu/arcgis/rest/services/TeamHermes/LawEnforcement/MapServer";
         const policeLayer = new MapImageLayer({
             url: policeLayerURL,
@@ -49,36 +65,169 @@ $(document).ready(function () {
             title: "EMS Locations in Utah County"
         });
 
-        // The map with basemap
-        const map = new Map({
-            basemap: "streets",
-        });
-
         map.add(policeLayer);
         map.add(fireStationLayer);
         map.add(emsLayer);
 
-        const view = new MapView({
-            container: "viewDiv",
-            map: map,
-            center: [-111.647070, 40.251468], //40.251468, -111.647070
-            zoom: 10
+        //////////////////////////////////////////////////////////////////////////// ADD DRAWING LAYERS
+        const pointLayer = new GraphicsLayer({
+            title: "Results Layer"
         });
-
-        // a graphics layer to show input point and output polygon
-        const graphicsLayer = new GraphicsLayer({
-            title: "User Input and Results Layer"
+        const obstructionLayer = new GraphicsLayer({
+            title: "User Drawn Obstructions"
         });
-        map.add(graphicsLayer);
+        const resultsLayer = new GraphicsLayer({
+            title: "User Drawn Obstructions"
+        });
+        map.add(pointLayer);
+        map.add(obstructionLayer);
+        map.add(resultsLayer);
 
+        // add the legend to the map
         const layerList = new LayerList({
             view: view,
         });
-
         view.ui.add(layerList, {
             position: "top-right",
         });
 
+
+        //////////////////////////////////////////////////////////////////////////// DRAWING CONTROLS
+        const draw = new Draw({
+            view: view
+        });
+        // add the control buttons to the map
+        view.ui.add("line-button", "top-left");
+        view.ui.add("point-button", "top-left");
+        view.ui.add("erase-button", "top-left");
+
+        // listeners for the 3 buttons
+        $("#line-button").click(function() {
+            // clear the layer then start drawing
+            view.graphics.removeAll();
+            const action = draw.create("polyline");
+            view.focus(); // focus the view to activate keyboard shortcuts for sketching
+
+            // listen polylineDrawAction events to give immediate visual feedback
+            // to users as the line is being drawn on the view.
+            action.on(
+                [
+                    "vertex-add",
+                    "vertex-remove",
+                    "cursor-update",
+                    "redo",
+                    "undo",
+                    "draw-complete"
+                ],
+                updateVertices
+            );
+        });
+
+        $("#point-button").click(function() {
+            view.on("click", addPoint);
+        });
+
+        $("#erase-button").click(function() {
+            view.graphics.removeAll();
+            pointLayer.removeAll();
+        });
+
+        //////////////////////////////////////////////////////////////////////////// FUNCTIONS FOR DRAWING A POLYLINE
+        // Checks if the last vertex is making the line intersect itself.
+        function updateVertices(event) {
+            // create a polyline from returned vertices
+            if (event.vertices.length > 1) {
+                const result = createGraphic(event);
+
+                // if the last vertex is making the line intersects itself,
+                // prevent the events from firing
+                if (result.selfIntersects) {
+                    event.preventDefault();
+                }
+            }
+        }
+        // create a new graphic presenting the polyline that is being drawn on the view
+        function createGraphic(event) {
+            view.graphics.removeAll();
+            // a graphic representing the polyline that is being drawn
+            const graphic = new Graphic({
+                geometry: {
+                    type: "polyline",
+                    paths: event.vertices,
+                    spatialReference: view.spatialReference
+                },
+                symbol: {
+                    type: "simple-line", // autocasts as new SimpleFillSymbol
+                    color: [4, 90, 141],
+                    width: 4,
+                    cap: "round",
+                    join: "round"
+                }
+            });
+            // check if the polyline intersects itself.
+            const intersectingSegment = getIntersectingSegment(graphic.geometry);
+            // Add a new graphic for the intersecting segment.
+            if (intersectingSegment) {
+                view.graphics.addMany([graphic, intersectingSegment]);
+            }
+            // Just add the graphic representing the polyline if no intersection
+            else {
+                view.graphics.add(graphic);
+            }
+            return {
+                selfIntersects: intersectingSegment
+            };
+        }
+        // function that checks if the line intersects itself
+        function isSelfIntersecting(polyline) {
+            if (polyline.paths[0].length < 3) {
+                return false;
+            }
+            const line = polyline.clone();
+
+            //get the last segment from the polyline that is being drawn
+            const lastSegment = getLastSegment(polyline);
+            line.removePoint(0, line.paths[0].length - 1);
+
+            // returns true if the line intersects itself, false otherwise
+            return geometryEngine.crosses(lastSegment, line);
+        }
+        // Checks if the line intersects itself. If yes, change the last
+        // segment's symbol giving a visual feedback to the user.
+        function getIntersectingSegment(polyline) {
+            if (isSelfIntersecting(polyline)) {
+                return new Graphic({
+                    geometry: getLastSegment(polyline),
+                    symbol: {
+                        type: "simple-line", // autocasts as new SimpleLineSymbol
+                        style: "short-dot",
+                        width: 3.5,
+                        color: "yellow"
+                    }
+                });
+            }
+            return null;
+        }
+        // Get the last segment of the polyline that is being drawn
+        function getLastSegment(polyline) {
+            const line = polyline.clone();
+            const lastXYPoint = line.removePoint(0, line.paths[0].length - 1);
+            const existingLineFinalPoint = line.getPoint(0, line.paths[0].length - 1);
+
+            return {
+                type: "polyline",
+                spatialReference: view.spatialReference,
+                hasZ: false,
+                paths: [
+                    [
+                        [existingLineFinalPoint.x, existingLineFinalPoint.y],
+                        [lastXYPoint.x, lastXYPoint.y]
+                    ]
+                ]
+            };
+        }
+
+        //////////////////////////////////////////////////////////////////////////// FUNCTIONS FOR DRAWING A POINT
         // symbol for input point
         const markerSymbol = {
             type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
@@ -88,7 +237,6 @@ $(document).ready(function () {
                 width: 2
             }
         };
-
         // symbol for buffered polygon
         const fillSymbol = {
             type: "simple-line", // autocasts as new SimpleFillSymbol()
@@ -99,14 +247,9 @@ $(document).ready(function () {
             // },
             width: 5,
         };
-
-        //add map click function
-        view.on("click", addPoint);
-
         //main function
         function addPoint(event) {
-
-            graphicsLayer.removeAll();
+            pointLayer.removeAll();
 
             const lat = event.mapPoint.latitude;
             const lon = event.mapPoint.longitude;
@@ -119,13 +262,15 @@ $(document).ready(function () {
                 geometry: point,
                 symbol: markerSymbol
             });
-            graphicsLayer.add(inputGraphic);
+            pointLayer.add(inputGraphic);
 
             $("#point-location").fadeIn();
             $("#lat").html("<p style='margin-left: 15px'>- Latitude: " + lat + "</p>");
             $("#lon").html("<p style='margin-left: 15px'>- Longitude: " + lon + "</p>");
         }
 
+
+        //////////////////////////////////////////////////////////////////////////// GEOPROCESSING FUNCTIONS
         // links to each of the 3 geoprocessing services
         const ems_gpurl = "http://geoserver2.byu.edu/arcgis/rest/services/TeamHermes/NetworkAnalystEMS/GPServer/NetworkAnalystEMS";
         const fire_gpurl = "http://geoserver2.byu.edu/arcgis/rest/services/TeamHermes/NetworkAnalystfirerouter/GPServer/NetworkAnalystfirerouter";
@@ -181,7 +326,7 @@ $(document).ready(function () {
                     geometry: point,
                     symbol: markerSymbol
                 });
-                graphicsLayer.add(inputGraphic);
+                pointLayer.add(inputGraphic);
                 const inputGraphicContainer = [];
                 inputGraphicContainer.push(inputGraphic);
                 const featureSet = new FeatureSet();
@@ -221,7 +366,7 @@ $(document).ready(function () {
             console.log("Drawing Shape");
             const polygon_feature = data.value.features[0];
             polygon_feature.symbol = fillSymbol;
-            graphicsLayer.add(polygon_feature);
+            pointLayer.add(polygon_feature);
 
             $("#loader").fadeOut();
             $("#save-results").fadeIn();
@@ -245,7 +390,7 @@ $(document).ready(function () {
             $.ajax({
                 url: "/apps/teamhermes/save_graphics_layer/", // the endpoint
                 type: "POST", // http method
-                data: JSON.stringify(graphicsLayer.graphics), // data sent with the post request, the form data from above
+                data: JSON.stringify(pointLayer.graphics), // data sent with the post request, the form data from above
                 dataType: "json",
 
                 // handle a successful response
@@ -254,7 +399,7 @@ $(document).ready(function () {
                         $("#save-results-status").html(`<p style="margin-left: 15px">Results Saved Successfully</p>`)
                     } else {
                         $("#save-results-status").html(`<p style="margin-left: 15px">
-                                                            The following error occured: ${resp.error_message}
+                                                            The following error occurred: ${resp.error_message}
                                                         </p>`)
                     }
                 },
